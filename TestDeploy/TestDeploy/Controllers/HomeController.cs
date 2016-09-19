b978ed7d456c4886da050da97bf89ebdb6d12012
sankargo <sankargo@SUPERPOWER>
2016-09-19 16:47:32 -0700

    Update HomeController

--- TestDeploy/TestDeploy/Controllers/HomeController.cs
+++ TestDeploy/TestDeploy/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestDeploy.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page 123";
 
             return View();
         }
